d954d71867af8b8d33122536c00fb284080aa496
wyf-ACCEPT <define_wifi@163.com>
2023-08-15 16:00:47 +0800

    functions for operator

--- contracts/core.sol
+++ contracts/core.sol
@@ -19,12 +19,13 @@ contract BigBond is Ownable, Pausable {
         uint256 pendingAmount;
         uint256 claimableAmount;
         AssetStatus status;
-        uint256 requestTimestamp;
+        uint256 lastRequestTimestamp;
     }
 
-    address public admin;       // The root admin of this contract
-    address public operator;    // The RWA operator role
     uint256 constant RATIFY_TIME_LIMIT = 7 days;
+    address public admin; // The root admin of this contract
+    address public operator; // The RWA operator role
+    uint256 public operatorDebt; // The amount of token that the operator needs to repay
     mapping(address => Asset) public userAssets;
     IERC20 public immutable tokenAddress;
 
@@ -35,11 +36,17 @@ contract BigBond is Ownable, Pausable {
     }
 
     /// Events for state transitions
-    event DepositEvent(address indexed user, uint256 amount, uint256 timestamp);
-    event RequestEvent(address indexed user, uint256 amount, uint256 timestamp);
-    event ClaimEvent(address indexed user, uint256 amount, uint256 timestamp);
-    event RatifyNormalEvent(address indexed user, uint256 amount, uint256 timestamp);
-    event RatifyExpiredEvent(address indexed user, uint256 amount, uint256 timestamp, uint256 extraTime);
+    event DepositEvent(address indexed user, uint256 amount);
+    event RequestEvent(address indexed user, uint256 amount);
+    event ClaimEvent(address indexed user, uint256 amount);
+    event RatifyNormalEvent(address indexed user, uint256 amount);
+    event RatifyExpiredEvent(
+        address indexed user,
+        uint256 amount,
+        uint256 extraTime
+    );
+    event borrowEvent(address indexed operator, uint256 amount);
+    event repayEvent(address indexed operator, uint256 amount);
 
     /// Modifiers for the state machine
     modifier stateIsNormal(address user) {
@@ -84,6 +91,10 @@ contract BigBond is Ownable, Pausable {
         return operator;
     }
 
+    function currentBalance() public view returns (uint256) {
+        return tokenAddress.balanceOf(address(this));
+    }
+
     /// Functions for users
     function depositAsset(uint256 amount) public amountNotZero(amount) {
         // Transfer token
@@ -92,7 +103,7 @@ contract BigBond is Ownable, Pausable {
         // Change the state
         Asset storage asset = userAssets[_msgSender()];
         asset.depositAmount += amount;
-        emit DepositEvent(_msgSender(), amount, block.timestamp);
+        emit DepositEvent(_msgSender(), amount);
     }
 
     function requestWithdraw(uint256 amount)
@@ -105,8 +116,8 @@ contract BigBond is Ownable, Pausable {
         asset.depositAmount -= amount;
         asset.pendingAmount += amount;
         asset.status = AssetStatus.Pending;
-        asset.requestTimestamp = block.timestamp;
-        emit RequestEvent(_msgSender(), amount, block.timestamp);
+        asset.lastRequestTimestamp = block.timestamp;
+        emit RequestEvent(_msgSender(), amount);
     }
 
     function claim() public stateIsClaimable(_msgSender()) {
@@ -118,33 +129,58 @@ contract BigBond is Ownable, Pausable {
 
         // Transfer token
         tokenAddress.transfer(_msgSender(), claimableAmount);
-        emit ClaimEvent(_msgSender(), claimableAmount, block.timestamp);
+        emit ClaimEvent(_msgSender(), claimableAmount);
     }
 
     /// Functions for operator
-    function ratifyWithin7days(address user, uint256 claimableAmount) public stateIsPending(user) {
+    function ratifyWithin7days(address user, uint256 claimableAmount)
+        public
+        stateIsPending(user)
+        onlyOperator
+    {
         Asset storage asset = userAssets[user];
         require(
-            block.timestamp <= asset.requestTimestamp + RATIFY_TIME_LIMIT, 
+            block.timestamp <= asset.lastRequestTimestamp + RATIFY_TIME_LIMIT,
             "Ratification time expired. Please call `ratifyExpired`."
         );
-        asset.claimableAmount = claimableAmount;    // Need to test this.
+        asset.claimableAmount = claimableAmount; // Need to test this.
         asset.pendingAmount = 0;
         asset.status = AssetStatus.Claimable;
-        emit RatifyNormalEvent(user, claimableAmount, block.timestamp);
+        emit RatifyNormalEvent(user, claimableAmount);
     }
 
-    function ratifyExpire(address user, uint256 claimableAmount) public stateIsPending(user) {
+    function ratifyExpire(address user, uint256 claimableAmount)
+        public
+        stateIsPending(user)
+        onlyOperator
+    {
         Asset storage asset = userAssets[user];
         require(
-            block.timestamp > asset.requestTimestamp + RATIFY_TIME_LIMIT, 
+            block.timestamp > asset.lastRequestTimestamp + RATIFY_TIME_LIMIT,
             "Ratification time isn't expired. Please call `ratifyWithin7days`."
         );
         asset.claimableAmount = claimableAmount;
         asset.pendingAmount = 0;
         asset.status = AssetStatus.Claimable;
-        uint256 extraTime = block.timestamp - asset.requestTimestamp - RATIFY_TIME_LIMIT;
-        emit RatifyExpiredEvent(user, claimableAmount, block.timestamp, extraTime);
+        uint256 extraTime = block.timestamp -
+            asset.lastRequestTimestamp -
+            RATIFY_TIME_LIMIT;
+        emit RatifyExpiredEvent(user, claimableAmount, extraTime);
     }
 
+    function borrowAssets(uint256 borrowAmount) public onlyOperator {
+        tokenAddress.transfer(operator, borrowAmount);
+        operatorDebt += borrowAmount;
+        emit borrowEvent(operator, borrowAmount);
+    }
+
+    function repayAssets(uint256 repayAmount) public onlyOperator {
+        require(
+            repayAmount <= operatorDebt,
+            "The repay amount should less than the debt!"
+        );
+        tokenAddress.transferFrom(operator, address(this), repayAmount);
+        operatorDebt -= repayAmount;
+        emit repayEvent(operator, repayAmount);
+    }
 }
